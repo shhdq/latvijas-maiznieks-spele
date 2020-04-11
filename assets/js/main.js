e08e581754be522e7a930045cfd598daf48834f6
Madars B <balodis.madars@gmail.com>
2020-04-11 23:21:57 +0300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
 const cards = document.querySelectorAll(".card");
+const red = document.getElementById("red-cover");
+
+red.addEventListener("click", () => {
+  alert("Ha ha");
+});
 
 function flipCard() {
   this.classList.toggle("is-flipped");
   console.log("test");
 }
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
