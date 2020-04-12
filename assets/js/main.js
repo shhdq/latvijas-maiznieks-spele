0ec3358aa48c09017d97390c25511ce35c8c890d
Madars B <balodis.madars@gmail.com>
2020-04-12 13:36:07 +0300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,47 @@ const body = document.getElementById("body");
 
 let gameTime = document.getElementById("game-time");
 
-function gameOver() {
-  alert("Game Over!");
-  body.style.backgroundImage = "none";
-  body.style.backgroundColor = "tomato";
-}
-// red.addEventListener("click", () => {
-//   alert("Spēle beigusies!");
-// });
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle("is-flipped");
+  this.classList.add("is-flipped");
 
-  setInterval(function () {
-    if (this) {
-      console.log("asdkjasd");
-    }
-  }, 2000);
+  if (!hasFlippedCard) {
+    // First Click
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  // Second Click
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
+}
+
+// Check For Match Function
+function checkForMatch() {
+  // Check Match
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  // Shorter if else
+  isMatch ? disableCards() : unFlipCards();
+}
+
+// Disable Cards
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+// Un Flip Cards
+function unFlipCards() {
+  // Not a Match
+  setTimeout(() => {
+    firstCard.classList.remove("is-flipped");
+    secondCard.classList.remove("is-flipped");
+  }, 1500);
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
